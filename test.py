e97eeee1d17b323e7906dd7d743e044b8ce22276
adomaitisc <caadomaitis@gmail.com>
2022-11-20 16:15:49 -0500

    .

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def verify_downloaded(file, path):
-    for files in os.walk(path):
-        print(files)
-        if file in files:
-            return True
-
-verify_downloaded('test.py', 'videos/')
